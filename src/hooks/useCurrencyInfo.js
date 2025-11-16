5518bf05d30d7d508f947bfbcb8545cee6a4c1d8
BijoyDeep11 <bijoydeep007@gmail.com>
2025-11-16 14:33:24 +0530

    fixed2

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -7,12 +7,9 @@ function useCurrencyInfo(currency) {
     fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
       .then((res) => res.json())
       .then((res) => {
-        setData(res[currency]);  // extract inner object
+        setData(res[currency]);
       })
-      .catch((err) => {
-        console.error("API error:", err);
-        setData({});
-      });
+      .catch(() => setData({}));
   }, [currency]);
 
   return data;
